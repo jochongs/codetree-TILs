ef50dcf9135a4e94d11ce04f349312641d66a135
jochong <joch2712@naver.com>
2024-02-22 21:36:28 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240222/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 13;
+    float b = 0.165;
+
+    cout << fixed;
+    cout << a << " * ";
+
+    cout.precision(6);
+    cout << b;
+
+    cout << " = ";
+    
+    cout.precision(6);
+    cout << a * b;
+
+    return 0;
+}
