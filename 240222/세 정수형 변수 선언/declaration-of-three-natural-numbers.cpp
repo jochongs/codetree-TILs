b9a413f846cff1c499c1bd716976d98a57ad8c65
jochong <joch2712@naver.com>
2024-02-22 21:27:49 +0900

    Title: 세 정수형 변수 선언, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240222/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int num1 = 7;
+    int num2 = 23;
+    int num3 = 30;
+
+    cout << num1 << " + " << num2 << " = " << num3;
+    
+    return 0;
+}
