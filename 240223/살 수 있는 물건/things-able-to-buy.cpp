d28a27710f39d4c457bb8a708d6e29ed47c3d290
jochong <joch2712@naver.com>
2024-02-23 18:29:42 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240223/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int money;
+
+    cin >> money;
+
+    if(money >= 3000) {
+        cout << "book";
+        return 0;
+    }
+
+    if(money >= 1000) {
+        cout << "mask";
+        return 0;
+    }
+
+    cout << "no";
+    return 0;
+}
