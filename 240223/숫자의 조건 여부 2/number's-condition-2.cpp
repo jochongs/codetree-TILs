3e44d44faa23dec1eb57596c93541edb67ee993b
jochong <joch2712@naver.com>
2024-02-23 18:39:50 +0900

    Title: 숫자의 조건 여부 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240223/숫자의 조건 여부 2/number's-condition-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a;
+
+    cin >> a;
+
+    if(a == 5) {
+        cout << "A";
+    }
+
+    if(b % 2 == 0) {
+        cout << "B";
+    }    
+
+    return 0;
+}
