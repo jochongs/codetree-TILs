0a2c87f714e62e068f69ee6af2a0a781645458bb
jochong <joch2712@naver.com>
2024-02-23 17:09:14 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240223/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int first, second;
+
+    cin >> first;
+    cin.get();
+    cin >> second;
+    cin.get();
+
+    cout << first;
+    cout << second;
+
+    return 0;
+}
