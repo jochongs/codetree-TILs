0110352f3f8273ab329954c5d4de2ae5ce6028fe
jochong <joch2712@naver.com>
2024-02-23 18:02:47 +0900

    Title: 시험 통과 여부 확인하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240223/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    
+    cin >> n;
+    
+    if(n >= 80) {
+        cout << "pass";
+        return 0;
+    }
+
+    cout << "more score";
+
+    return 0;
+}
