8ad025546c038a13bf1d1e2937c8cb2a2a0c3a72
jochong <joch2712@naver.com>
2024-02-23 17:04:42 +0900

    Title: 1시간 뒤 시간 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240223/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int hour, min;
+    char c;
+
+    cin >> hour >> c >> min;
+
+    cout << hour + 1 << ":" << min;
+
+    return 0;
+}
