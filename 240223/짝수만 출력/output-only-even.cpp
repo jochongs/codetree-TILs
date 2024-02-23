8e6a8ec3f61585a560d342d42118f39827427f7a
jochong <joch2712@naver.com>
2024-02-23 19:59:24 +0900

    Title: 짝수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240223/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b;
+    
+    cin >> a >> b;
+
+    if (a % 2 != 0) {
+        a += 1;
+    }
+
+    for (int i = a; i <= b; i += 2) {
+        cout << i << ' ';
+    }
+
+    return 0;
+}
