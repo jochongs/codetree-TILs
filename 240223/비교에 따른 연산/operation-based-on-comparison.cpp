e4a706dc80a37866c6a713599d1b60aeec6ed516
jochong <joch2712@naver.com>
2024-02-23 18:16:18 +0900

    Title: 비교에 따른 연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240223/비교에 따른 연산/operation-based-on-comparison.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b;
+
+    cin >> a >> b;
+
+    if(a > b) {
+        cout << a * b;
+        return 0;
+    }
+
+    cout << b % a;
+
+    return 0;
+}
