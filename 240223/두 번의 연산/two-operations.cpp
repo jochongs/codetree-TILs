766c97e9bcbfbfe018cb0d1c7d12c0dd063f6c55
jochong <joch2712@naver.com>
2024-02-23 18:41:26 +0900

    Title: 두 번의 연산, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240223/두 번의 연산/two-operations.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a;
+
+    cin >> a;
+
+    if(a % 2 != 0) {
+        a += 3;
+    }
+
+    if(a % 3 == 0) {
+        a /= 3;
+    }
+
+    cout << a;
+    
+    return 0;
+}
