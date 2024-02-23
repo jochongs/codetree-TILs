56c4ac0f8cbf4c2219e1c185bec5d612fa35104e
jochong <joch2712@naver.com>
2024-02-23 17:32:42 +0900

    Title: 입력받아 연산하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240223/입력받아 연산하기/take-input-and-operate.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+ios_base :: sync_with_stdio(false);
+cin.tie(NULL); 
+cout.tie(NULL);
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    
+    cin >> a >> b;
+
+    cout << a + b << "\n";
+    cout << b / 10 << "\n";
+
+    return 0;
+}
