12c6e7daee400d81377fc3640d88d5dcca647b3a
jochong <joch2712@naver.com>
2024-02-23 17:33:42 +0900

    Title: 입력받아 연산하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240223/입력받아 연산하기/take-input-and-operate.cpp
+++ 240223/입력받아 연산하기/take-input-and-operate.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-ios_base :: sync_with_stdio(false);
-cin.tie(NULL); 
-cout.tie(NULL);
-
 int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL); 
+    cout.tie(NULL);
+
     // 여기에 코드를 작성해주세요.
     int a, b;
     
     cin >> a >> b;
 
-    cout << a + b << "\n";
-    cout << b / 10 << "\n";
+    cout << a + 87 << "\n";
+    cout << b % 10 << "\n";
 
     return 0;
 }
