185c208d8dac8f62c1a68f15b7b7b31cb4cf3008
jochong <joch2712@naver.com>
2024-02-23 18:31:16 +0900

    Title: 출석 부르기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240223/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int studentNum;
+
+    cin >> studentNum;
+
+    if(studentNum == 1) {
+        cout << "John";
+        return 0;
+    }
+
+    if(studentNum == 2) {
+        cout << "Tom";
+        return 0;
+    }
+
+    if(studentNum == 3) {
+        cout << "Paul";
+        return 0;
+    }
+
+    return 0;
+}
