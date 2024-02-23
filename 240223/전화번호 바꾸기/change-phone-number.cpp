a2cdaa22cbb4371dc737e3647a291b3afb8e0db3
jochong <joch2712@naver.com>
2024-02-23 17:11:58 +0900

    Title: 전화번호 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240223/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int xxxx, yyyy;
+    int temp;
+
+    cin >> temp;
+    cin.get();
+
+    cin >> xxxx;
+    cin.get();
+
+    cin >> yyyy;
+    cin.get();
+
+    cout << "010" << '-' << yyyy << '-' << xxxx;
+ 
+    return 0;
+}
