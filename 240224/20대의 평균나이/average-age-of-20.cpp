8b124056a6289ee4ad71025246400aca928c6daa
jochong <joch2712@naver.com>
2024-02-24 23:21:58 +0900

    Title: 20대의 평균나이, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240224/20대의 평균나이/average-age-of-20.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int totalSum = 0;
+    int cnt = 0;
+    while (true) {
+        int age = 0;
+
+        cin >> age;
+
+        if (age < 20) {
+            break;
+        }
+
+        totalSum += age;
+        cnt++;
+    }
+
+    cout << totalSum / double(cnt); 
+
+    return 0;
+}
