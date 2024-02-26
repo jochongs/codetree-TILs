d4505aede60fab4fd851f3802df0bfc395190f92
jochong <joch2712@naver.com>
2024-02-26 15:29:49 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240226/원소 10개의 합/sum-of-10-elements.cpp
+++ 240226/원소 10개의 합/sum-of-10-elements.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int arr[10];
 
     for (int i = 0; i < 10; i++) {
@@ -15,6 +20,7 @@ int main() {
     for (int i = 0; i < 10; i++) {
         totalSum += arr[i];
     }
+    cout << totalSum;
 
     return 0;
 }
