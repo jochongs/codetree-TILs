bb52563b58cb32cfa5e8453dd7894235c6c7ff7d
jochong <joch2712@naver.com>
2024-02-26 15:20:50 +0900

    Title: 알파벳 출력하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240226/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int alphabetASCII = 65;
+    for (int i = 0; i < n; i++) {
+        // 공백
+        for (int j = 0; j < i; j++) {
+            cout << "  ";
+        }
+
+        // 알파벳
+        for (int j = 0; j < n - i; j++) {
+            char alphabet = alphabetASCII;
+            cout << alphabet << ' ';
+            alphabetASCII++;
+        }
+
+        cout << '\n';
+    }
+
+    return 0;
+}
