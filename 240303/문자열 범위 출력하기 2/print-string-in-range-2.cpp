6314e993e337668095517bb1f64fd48d41ab022f
jochong <joch2712@naver.com>
2024-03-03 13:43:57 +0900

    Title: 문자열 범위 출력하기 2, Time: 20ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240303/문자열 범위 출력하기 2/print-string-in-range-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string str;
+    int num;
+
+    cin >> str >> num;
+
+    if (str.length() < num) {
+        num = str.length();
+    }
+
+    for (int i = str.length() - 1; i >= str.length() - num; i--) {
+        cout << str[i];
+    }
+
+    return 0;
+}
