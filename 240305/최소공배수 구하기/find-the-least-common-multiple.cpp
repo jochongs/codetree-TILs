5322b48dc0c813fdb3d297546caefee65342dd17
jochong <joch2712@naver.com>
2024-03-05 13:23:42 +0900

    Title: 최소공배수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240305/최소공배수 구하기/find-the-least-common-multiple.cpp
+++ 240305/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int* divisor(int n, int& size) {
-    int arr[100] = {0, };
+    int* arr = new int[100];
 
     int index = 0;
-    for (int i = 1; i < n; i++) {
-        if (n % i != n) continue;
+    for (int i = 1; i <= n; i++) {
+        if (n % i != 0) continue;
 
         arr[index] = i;
         index++;
@@ -16,17 +16,36 @@ int* divisor(int n, int& size) {
     return arr;
 }
 
+int gcl(int n, int m) {
+    int nSize = 0;
+    int mSize = 0;
+    int* nDivisor = divisor(n, nSize);
+    int* mDivisor = divisor(m, mSize);
+
+    int gcl = 1;
+    for (int i = 0; i < nSize; i++) {
+        for (int j = 0; j < mSize; j++) {
+            if (nDivisor[i] == mDivisor[j]) {
+                gcl = nDivisor[i];
+            }
+        }
+    }
+
+    return gcl;
+}
 
 int gcf(int n, int m) {
+    int gclNum = gcl(n, m);
 
-    return 1;
-    
+    return (n / gclNum) * (m / gclNum) * gclNum;
 }
 
 int main() {
     // 여기에 코드를 작성해주세요.
     int n, m;   
     cin >> n >> m;
+
+    cout << gcf(12, 18);
 
     return 0;
 }
